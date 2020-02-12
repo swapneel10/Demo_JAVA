7ba7782187934b3d38f2d6c9b3286225c2206d67
Swapneel <sng@infoblox.com>
2020-02-12 11:47:16 +0530

    JAVA FILES

---
+++ src/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.srccodes.example;
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World !!\n");
+		Scanner input=new Scanner(System.in);
+		int a=input.nextInt();		
+		System.out.println(2*a);
+	}
+
+}
